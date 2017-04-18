f6d1524b4353b4d0f0d4bdc140219c4bffb4711c
Robert Obreczarek <robert@obreczarek.com>
2017-04-18 22:53:21 +0200

    Wrapping up Section 6, Lecture 88

---
+++ src/components/posts_new.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
+
+  render() {
+    
+    const { 
+      fields: { title, categories, content }, 
+      handleSubmit 
+    } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.props.createPost)}>
+          <h3>Create a New Post</h3>
+
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control" {...title} />
+            <div className="text-help">
+              { title.touched ? title.error : '' }
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label>Categories</label>
+            <input type="text" className="form-control" {...categories} />
+          </div>
+
+          <div className="form-group">
+            <label>Content</label>
+            <textarea className="form-control" {...content} />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'PostsNew',
+  fields: ['title', 'categories', 'content'],
+  validate
+}, null, { createPost })(PostsNew);
